9ca12cd39e1864e890a3f98f5b4d2dea0dc2f69f
Asem Naderi <dev.asem.naderi@gmail.com>
2025-03-28 16:58:29 +0430

    change size of png-files and edit css

--- src/components/bodySide/BodySide.jsx
+++ src/components/bodySide/BodySide.jsx
@@ -2,7 +2,7 @@ import Part from '../part/Part';
 
 function BodySide({ sideParts, onClick }) {
     return (
-      <div className="bg-fuchsia-600 min-h-[540px] rounded-md grid grid-cols-3 grid-rows-8 w-80 relative">
+      <div className="bg-fuchsia-600 min-h-[800px] rounded-md grid grid-cols-[145px_145px_145px] grid-rows-8 relative">
         {sideParts.map(part => <Part
           key={part.id}
           partSrc={part.src}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
-module.exports = {
-    content: [],
+// module.exports = {
+//     content: [],
+//     theme: {
+//         extend: {},
+//     },
+//     plugins: []
+// }
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{html,js}"],
     theme: {
         extend: {},
     },
-    plugins: []
+    plugins: [],
 }
